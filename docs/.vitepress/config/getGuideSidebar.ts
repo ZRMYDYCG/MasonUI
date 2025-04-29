1037e3e0205507082235030c5dc337a7ae432d53
ZRMYDYCG <547471919@qq.com>
2025-04-29 20:10:40 +0800

    docs: 📖️优化文档路径

--- docs/.vitepress/config/getGuideSidebar.ts
+++ docs/.vitepress/config/getGuideSidebar.ts
@@ -5,19 +5,19 @@ export function getGuideSidebar(): DefaultTheme.Sidebar {
     {
       text: '指南',
       items: [
-        { text: '基本介绍', link: '' },
-        { text: '安装', link: '' },
-        { text: '快速开始', link: '' },
-        { text: '暗黑模式', link: '' },
-        { text: '更新日志', link: '' },
+        { text: '基本介绍', link: '/guide/basic/' },
+        { text: '安装', link: '/guide/install/' },
+        { text: '快速开始', link: '/guide/quickstart/' },
+        { text: '暗黑模式', link: '/guide/dark-mode/' },
+        { text: '更新日志', link: '/guide/logs/' },
       ],
     },
     {
       text: '进阶',
       items: [
-        { text: '主题', link: '' },
-        { text: '无障碍', link: '' },
-        { text: '指令', link: '' },
+        { text: '主题', link: '/guide/theme/' },
+        { text: '无障碍', link: '/guide/accessibility/' },
+        { text: '指令', link: '/guide/directive/' },
       ],
     },
   ]
--- docs/.vitepress/config/getNav.ts
+++ docs/.vitepress/config/getNav.ts
@@ -2,10 +2,10 @@ import type { DefaultTheme } from 'vitepress'
 
 export function getNav(): DefaultTheme.NavItem[] {
   return [
-    { text: '指南', link: '/guide/' },
+    { text: '指南', link: '/guide/basic/' },
     { text: '设计', link: '/design/color/' },
-    { text: '组件', link: '/components/layout/' },
-    { text: '指令', link: '/directives/ripple/ ' },
+    { text: '组件', link: '/components/button/' },
+    { text: '指令', link: '/directives/ripple/' },
     { text: '联系我们', link: '/contact/ ' },
   ]
 }
