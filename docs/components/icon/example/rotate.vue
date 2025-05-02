144bdf40ceaeaa47224c57a93e18c71e3545c512
ZRMYDYCG <547471919@qq.com>
2025-05-02 14:28:00 +0800

    docs: 📖️（m-icon）完善文档

---
+++ docs/components/icon/example/rotate.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="icon-demo">
+    <MIcon name="Check" spin />
+    <MIcon name="Close" spin />
+    <MIcon name="ArrowLeft" spin />
+  </div>
+</template>
+
+<style scoped>
+.icon-demo {
+  display: flex;
+  gap: 20px;
+}
+</style>
